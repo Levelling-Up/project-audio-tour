9aae9a726e86b80b9d40e6621d40859cc2fab05b
Jethet <henriette.hettinga@protonmail.com>
2021-06-01 14:10:22 +0200

    Created new branch welcome-page-hh to code the welcome page

--- client/src/screens/Welcome.js
+++ client/src/screens/Welcome.js
@@ -2,7 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 function Welcome(props) {
-  return <></>;
+  return (
+    <Container>
+      <Logo>
+        <h3>Audio Tour Logo</h3>
+      </Logo>
+      <Welcome>
+        <h2>Welcome to Audio Tours</h2>
+      </Welcome>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+  width: 50%;
+  height: 85px;
+  background: blue;
+  border-radius: 100px / 50px;
+`;
 
 export default Welcome;
