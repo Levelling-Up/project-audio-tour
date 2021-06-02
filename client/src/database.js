f3ba78f5332bd794dd9e50d4f792d8f5588f5164
ecalogero <ecalogero@gmail.com>
2021-06-02 17:58:18 +0100

    created branch for the tracks DB on the frontend

---
+++ client/src/database.js
@@ -0,0 +1,80 @@
+export const tracksDB = [
+    {id:"1",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-1_6738168_1621775430.mp3",
+        poi_id: "1",
+        language: "english"
+    },
+    {id:"2",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-2_6738427_1621776407.mp3",
+        poi_id: "2",
+        language: "english"
+    },
+    {id:"3",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-3_6738535_1621776749.mp3",
+        poi_id: "3",
+        language: "english"
+    },
+    {id:"4",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-4_6738636_1621777076.mp3",
+        poi_id: "4",
+        language: "english"
+    },
+    {id:"5",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-5_6738824_1621777913.mp3",
+        poi_id: "5",
+        language: "english"
+    },
+    {id:"6",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-6_6738946_1621778469.mp3",
+        poi_id: "6",
+        language: "english"
+    },
+    {id:"7",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-7_6739078_1621778983.mp3",
+        poi_id: "7",
+        language: "english"
+    },
+    {id:"8",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-8_6739257_1621779696.mp3",
+        poi_id: "8",
+        language: "english"
+    },
+    {id:"9",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-9_6739411_1621780285.mp3",
+        poi_id: "9",
+        language: "english"
+    },
+    {id:"10",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-10_6739577_1621780863.mp3",
+        poi_id: "10",
+        language: "english"
+    },
+    {id:"11",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-11_6739701_1621781297.mp3",
+        poi_id: "11",
+        language: "english"
+    },
+    {id:"12",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-12_6739797_1621781750.mp3",
+        poi_id: "12",
+        language: "english"
+    },
+    {id:"13",
+        image_url: "https://unsplash.com/photos/tbEAfHcV8n4",
+        audio_url: "https://canaltouraudiofiles.s3.eu-west-2.amazonaws.com/Victoria-English-Track-13_6739940_1621782318.mp3",
+        poi_id: "13",
+        language: "english"
+    }
+]
