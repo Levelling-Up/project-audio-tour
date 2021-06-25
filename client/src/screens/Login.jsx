b1a3eda6156eb464e4be87c5bc110b8cb7a7adb7
Jethet <henriette.hettinga@protonmail.com>
2021-06-25 17:50:01 +0200

    Create branch for new buttons; styling

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Login(props) {
   return (
@@ -18,6 +19,7 @@ function Login(props) {
           placeholder="Access code"
           autoComplete="off"
         ></AccessCode>
+        <SubmitButton to="/tours">Submit</SubmitButton>
 
         <Text>Or scan your QR code here:</Text>
       </Group>
@@ -39,7 +41,7 @@ const Logo = styled.div`
   margin-top: 50px;
   width: 55%;
   height: 95px;
-  background: blue;
+  background-color: rgb(52, 152, 219);
   font-size: 26px;
   color: whitesmoke;
   border-radius: 100px/80px;
@@ -57,11 +59,11 @@ const AccessCodeLabel = styled.label`
   font-family: Arial;
   height: 70px;
   width: 265px;
-  color: blue;
+  color: rgb(52, 152, 219);
   text-align: center;
   font-size: 30px;
   margin-top: 50px;
-  margin-bottom: 40px;
+  margin-bottom: 30px;
   /* border: 1px solid red; */
 `;
 
@@ -70,18 +72,28 @@ const AccessCode = styled.input`
   font-family: Arial;
   height: 40px;
   width: 250px;
-  color: blue;
+  color: rgb(52, 152, 219);
   /* text-align: left; */
   font-size: 20px;
   margin-top: 40px;
   padding: 12px;
-  border: 1px solid blue;
+  border: 2px solid rgb(52, 152, 219);
+`;
+
+const SubmitButton = styled(Link)`
+  font-weight: bolder;
+  text-decoration: none;
+  background-color: rgb(40, 116, 166);
+  color: whitesmoke;
+  padding: 10px;
+  margin-top: 20px;
+  border-radius: 50px;
 `;
 
 const Text = styled.span`
   display: flex;
   font-family: Arial;
-  color: blue;
+  color: rgb(52, 152, 219);
   text-align: center;
   width: 260px;
   font-size: 30px;
